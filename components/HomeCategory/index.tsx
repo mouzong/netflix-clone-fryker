88fb0a2f5c3a2e9a812e820ef22b1185e062c784
mouzong <andreasmouzong@gmail.com>
2021-04-17 15:23:14 +0100

    added navigation between pages

--- components/HomeCategory/index.tsx
+++ components/HomeCategory/index.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
-import { Image, FlatList } from "react-native";
+import { Image, FlatList, Pressable } from "react-native";
 import styles from "./styles";
 import { Text } from "../../components/Themed";
+import { useNavigation } from "@react-navigation/native";
+import movie from "../../assets/data/movie";
 
 interface HomeCategoryProps {
   category: {
@@ -13,18 +15,26 @@ interface HomeCategoryProps {
 
 const HomeCategory = (props: HomeCategoryProps) => {
   const { category } = props;
+  const navigation = useNavigation();
+
+  const onMoviePress = (movie) => {
+    navigation.navigate("MovieDetailsScreen", { id: movie.id });
+  };
+
   return (
     <>
       <Text style={styles.title}>{category.title}</Text>
       <FlatList
         data={category.movies}
         renderItem={({ item }) => (
-          <Image
-            style={styles.image}
-            source={{
-              uri: item.poster,
-            }}
-          />
+          <Pressable onPress={() => onMoviePress(item)}>
+            <Image
+              style={styles.image}
+              source={{
+                uri: item.poster,
+              }}
+            />
+          </Pressable>
         )}
         horizontal
         showsHorizontalScrollIndicator={false}
